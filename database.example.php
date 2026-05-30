1a837e545c55191bbbc7946034b3be2fbafa88e5
Karl Indra <indy1@gmx.at>
2026-05-30 16:22:09 +0200

    chore: add database config and gitignore

---
+++ database.example.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host'     => 'localhost',
+    'dbname'   => 'mealana_erp',
+    'username' => 'root',
+    'password' => 'HIER_DEIN_PASSWORT',
+    'charset'  => 'utf8mb4'
+];
